00703cd4a9007277ab4b30270780fde850d6e92e
ErrorErrorError <erikbautista15@gmail.com>
2021-03-24 18:23:35 -0500

    Updated to support getting files from common-services, Added readme and base

---
+++ README.md
@@ -0,0 +1,29 @@
+# Team 22 Supply Back end
+This is the Team 22 backend for supply services. This repository deals with any requests handled in `https://supply.team22.sweispring21.tk/api/v1/[request-name]` for `supply` services.
+
+
+# Modifying This Repo
+### Cloning repository
+***Before you star you must have Python 3.8 installed in your system***  
+If you would like to contribute to this repository, you first must clone this repository by running:  
+```git clone https://bitbucket.org/swe-spring-2021-team-22/team22-supply-backend.git```  
+  
+### Setting Up Environment
+After doing so, go to the `team22-supply-backend` directory using command line or PyCharm Terminal and we will install the `env` environment for your setup by running:  
+`python3 -m venv env`  
+  
+### Activating Environment
+Now that you have the environment, in order to be in the environment you type:  
+`source env/bin/activate`  
+  
+### Installing/Uninstalling Dependencies
+Make sure you install dependencies. You do so by running `python3 -m pip install -r requirements.txt`. If you added more or removed dependencies and need to generate a new `requirements.txt`, you do so by running `pip freeze > requirements.txt`.
+
+#### .env file ####
+This file you have to make on your own. It should be on the project directory. If you notice, `MongoUtils.py` script uses `MONGO_SECRET`, which needs to be defined in `.env` file. This holds our mongo database `developer` password.
+
+### Deactivating Environment
+Now you should be in the `env` environment. To get out of the environment you type `deactivate` in command line.
+
+
+### MAKE SURE IF YOU CLONE THIS THAT THE PARENT DIRECTORY ALSO HAS `team22-common-services-backend`.
---
+++ server.py
@@ -0,0 +1,61 @@
+import sys
+# Import common services backend to access database
+sys.path.insert(1, '../team22-common-services-backend')
+
+import json
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+from MongoUtils import initMongoFromCloud
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    version = '0.0.1'
+
+    # Reads the POST data from the HTTP header
+    def extract_POST_Body(self):
+        postBodyLength = int(self.headers['content-length'])
+        postBodyString = self.rfile.read(postBodyLength)
+        postBodyDict = json.loads(postBodyString)
+        return postBodyDict
+
+    # handle post requests
+    def do_POST(self):
+        status = 404  # HTTP Request: Not found
+        postData = self.extract_POST_Body()  # store POST data into a dictionary
+        path = self.path
+        cloud = 'supply'
+        client = initMongoFromCloud(cloud)
+        db = client['team22_' + cloud]
+        responseBody = {
+            'status': 'failed',
+            'message': 'Request not found'
+        }
+
+        if '/request-name' in path:
+            # Handle request here
+            print('Handle request here')
+
+        self.send_response(status)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        responseString = json.dumps(responseBody).encode('utf-8')
+        self.wfile.write(responseString)
+        client.close()
+
+    def do_GET(self):
+        return
+
+
+def main():
+    port = 4001  # Port 4001 reserved for demand backend
+    server = HTTPServer(('', port), SimpleHTTPRequestHandler)
+    print('Server is starting... Use <Ctrl+C> to cancel. Running on Port {}'.format(port))
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("Stopped server due to user interrupt")
+    print("Server stopped")
+
+
+if __name__ == "__main__":
+    main()
